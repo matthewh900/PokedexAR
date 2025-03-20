b56674c63d5fe775a0fd3ff743bfaf86944134eb
matthewh900 <matthewh900@gmail.com>
2025-03-20 12:23:24 +0000

    add improved functionality to fetch and display data

--- Database/PokeApi.tsx
+++ Database/PokeApi.tsx
@@ -6,6 +6,12 @@ export function GetAllPokemon() {
     })
 }
 
+export function GetNext(next: string) {
+    return axios.get(next).then((res) => {
+        return res
+    })
+}
+
 export function GetPokemonData() {
     return axios.get("https://pokeapi.co/api/v2/pokemon/1").then((res) => {
         return res
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,47 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, SafeAreaView, ActivityIndicator } from "react-native";
 import styles from "./styles";
 import { useEffect, useState } from "react";
-import { GetAllPokemon } from "@/Database/PokeApi";
+import { GetAllPokemon, GetNext } from "@/Database/PokeApi";
 import { PokemonCard } from "@/components/PokemonCard";
 
+interface Pokemon {
+  name: string,
+  url: string
+}
+
 export default function Index() {
-  const [pokemon, setPokemon] = useState<object>({})
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [next, setNext] = useState<string>()
+  const [isLoadingMore, setIsLoadingMore] = useState(false)
 
   useEffect(() => {
     GetAllPokemon().then((res) => {
-      setPokemon(res.data.results[0])
-    })
-  }, [])
+      setPokemon(res.data.results);
+      setNext(res.data.next)
+    });
+  }, []);
+
+  const loadMore = () => {
+    if (isLoadingMore) return
+    if (next){
+      setIsLoadingMore(true)
+      GetNext(next).then((res) => {
+        setPokemon(prevPokemon => ([...prevPokemon, ...res.data.results]));
+        setNext(res.data.next)
+        setIsLoadingMore(false)
+      })
+    }
+  }
 
   return (
-    <View
-      style={styles.IndexView}
-    >
-      <PokemonCard pokemon={pokemon}/>
-    </View>
+    <SafeAreaView style={styles.IndexView}>
+      <FlatList
+        data={pokemon}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        onEndReached={loadMore}
+        ListFooterComponent={() => isLoadingMore ? <ActivityIndicator/> : null}
+      />
+    </SafeAreaView>
   );
 }
