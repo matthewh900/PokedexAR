2b1a083c4f3af487785cab8456baafe25627e9f0
matthewh900 <matthewh900@gmail.com>
2025-03-24 13:07:43 +0000

    move code from index file into home file

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,7 @@
-import { FlatList, Text, SafeAreaView, ActivityIndicator } from "react-native";
-import styles from "./styles";
-import { useEffect, useState } from "react";
-import { GetAllPokemon, GetNext } from "@/Database/PokeApi";
-import { PokemonCard } from "@/components/PokemonCard";
-
-interface Pokemon {
-  name: string,
-  url: string
-}
+import { Home } from "@/screens/Home";
 
 export default function Index() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  const [next, setNext] = useState<string>()
-  const [isLoadingMore, setIsLoadingMore] = useState(false)
-
-  useEffect(() => {
-    GetAllPokemon().then((res) => {
-      setPokemon(res.data.results);
-      setNext(res.data.next)
-    });
-  }, []);
-
-  const loadMore = () => {
-    if (isLoadingMore) return
-    if (next){
-      setIsLoadingMore(true)
-      GetNext(next).then((res) => {
-        setPokemon(prevPokemon => ([...prevPokemon, ...res.data.results]));
-        setNext(res.data.next)
-        setIsLoadingMore(false)
-      })
-    }
-  }
-
   return (
-    <SafeAreaView style={styles.IndexView}>
-      <FlatList
-        data={pokemon}
-        keyExtractor={(item) => item.name}
-        renderItem={({ item }) => <PokemonCard url={item.url}/>}
-        onEndReached={loadMore}
-        ListFooterComponent={() => isLoadingMore ? <ActivityIndicator/> : null}
-      />
-    </SafeAreaView>
-  );
+    <Home />
+  )
 }
